3bf0154795514543a14cc7a57449322f2e8c1b6f
muhammadabubakar1234 <abubakar60675@gmail.com>
2024-06-12 19:50:52 +0500

    Update QuestionCard.tsx

--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -53,7 +53,7 @@ const QuestionCard = ({
         </div>
 
         <div className="mt-3.5 flex flex-wrap gap-2">
-          {tags.map((tag) => (
+          {tags.map((tag: any) => (
             <RenderTag key={tag._id} _id={tag._id} name={tag.name} />
           ))}
         </div>
